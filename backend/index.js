1f7e3060253ed501da2a3dfe788fdfd167a39d86
Huzaifa Farooq <huzaifa1947@gmail.com>
2026-08-01 01:20:48 +0500

    feat: add JWT authentication utilities

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const jwt = require("jsonwebtoken");
+const{ authenticateToken } = require("./utilities");
+
 app.use(express.json());
 
 app.use(
