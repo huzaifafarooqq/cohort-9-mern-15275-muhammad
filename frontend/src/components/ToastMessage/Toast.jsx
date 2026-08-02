14677a0d0faedb5f346558a7e760f5837eebf716
Huzaifa Farooq <huzaifa1947@gmail.com>
2026-08-02 20:11:57 +0500

    feat: integrate note deletion and add empty notes state

--- frontend/src/components/ToastMessage/Toast.jsx
+++ frontend/src/components/ToastMessage/Toast.jsx
@@ -3,6 +3,7 @@ import { LuCheck } from "react-icons/lu";
 import { MdDeleteOutline } from "react-icons/md";
 
 const Toast = ({ isShown, message, type, onClose }) => {
+  console.log(type);
   useEffect(() => {
     if (!isShown) return;
 
